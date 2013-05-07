70cf08c25c72d4c15725b60c1f61d09e549748ca
elju <moresilenter@gmail.com>
2013-05-07 14:05:35 -0500

    modified:   public/javascripts/main.js 	modified:   public/stylesheets/art.css 	modified:   public/stylesheets/art.less 	modified:   public/stylesheets/blog.css 	modified:   public/stylesheets/blog.less 	modified:   public/stylesheets/lju.css 	modified:   public/stylesheets/lju.less 	modified:   public/stylesheets/main.css 	modified:   public/stylesheets/mixins.css 	modified:   public/stylesheets/mixins.less 	modified:   public/stylesheets/navbar.css 	modified:   public/stylesheets/navbar.less 	modified:   views/art.jade 	modified:   views/blog.jade 	modified:   views/index.jade 	modified:   views/navbar.jade

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,82 +1,68 @@
-/*
- *
- * $(function() {
- *   console.time("setting up click events.");
- *   $(".navbutton").each(function(i, el) {
- *       that = this;
- *       $(this).on('click', function(e) {
- *           e.preventDefault();
- *           history.pushState({}, this.id, this.href);
- *           $mainbar = $('.mainbar');
- *           $mainbar.hasClass('activated') ? $mainbar.removeClass('activated') : $mainbar.addClass('activated');
- *           return false;
- *       })
- *   });
- *   console.timeEnd("setting up click events.");
- * });
-
-*/
-
-$(function() {
-    var $slices;
+$(document).on('ready', function() {
+    var $slices, $artwork, $activeHeap, undoInvert, pullUpViewer, Inverter;
     $slices = $('.slice');
     $slices.on('click', function alternate (e) {
+        console.log(e);
+        if (!$(document.documentElement).hasClass('inverted')) { 
 
-        e.stopPropagation();
-        var $this, $right, $left, $others;
+            e.stopPropagation();
+            var $this, $right, $left, $others;
 
-        if (window.location.hash && window.location.hash !== '#') {
-            $('#warp').removeClass('go goone gotwo gothree gofour')
-            $('.slice').removeClass('right1 right2 right3 left1 left2 left3');
-            $('.subwindow').css('overflow-y', 'inherit');
-            window.location.hash = '';
-            e.preventDefault();
+            if (window.location.hash && window.location.hash !== '#') {
+                $('#warp').removeClass('go goone gotwo gothree gofour')
+                $('.slice').removeClass('right1 right2 right3 left1 left2 left3');
+                $('.subwindow').css('overflow-y', 'inherit');
+                window.location.hash = '';
+                e.preventDefault();
+            } else {
+                $right = $slices.filter('#' + this.id + ' ~ a');
+                $left = $slices.not($right).not(this);
+                $left.each(function(i) {
+                    $(this).addClass('left' + ($left.length - i));} );
+                $right.each(function(i) {
+                    $(this).addClass('right' + (i + 1)); });
+                $('#warp').addClass('go').addClass('go' + this.id);
+                var that = this;
+                ['transitionend', 'webkitTransitionEnd', 'oTransitionEnd'].forEach(function (el, i) {
+                    $(that).on(el, function() {$(this).children().css('overflow-y', 'auto');}); } );
+
+            }
         } else {
-            $right = $slices.filter('#' + this.id + ' ~ a');
-            $left = $slices.not($right).not(this);
-            $left.each(function(i) {
-                $(this).addClass('left' + ($left.length - i));} );
-            $right.each(function(i) {
-                $(this).addClass('right' + (i + 1)); });
-            $('#warp').addClass('go').addClass('go' + this.id);
-            var that = this;
-            ['transitionend', 'webkitTransitionEnd', 'oTransitionEnd'].forEach(function (el, i) {
-                $(that).on(el, function() {$(this).children().css('overflow-y', 'auto');}); } );
-
+            e.preventDefault();
         }
     } );
+
+    Inverter =
+    {
+        pullUpViewer : function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            var $this = $(this);
+            $('.artwork').off('click');
+            ($activeHeap = $this.parent().parent()).addClass('art-front-and-center');     // This triggers the necessary classes to invert everything else.
+            $(document.documentElement).addClass('inverted');
+            $(document.body).on('click', Inverter.undoInvert);
+            var $artscont = $('#one > .subwindow');
+            $('<div>').attr('id','screen').height($("#arts-container").height()).click(
+                function(e) {
+                    $(this).off('click').remove();
+                    $('.art-front-and-center').removeClass('art-front-and-center');
+                    $(document.documentElement).removeClass('inverted');
+                    $('.artwork').click(pullUpViewer);
+                    return null;
+                }).appendTo($artscont);
+        },
+
+        undoInvert : function undoInvert (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $(this).off('click');
+            $('.artwork').on('click', Inverter.pullUpViewer).parent().parent().removeClass('art-front-and-center');
+            $(document.documentElement).removeClass('inverted'); 
+            return false;
+        }
+    };
+
+    $('.artwork').on('click', Inverter.pullUpViewer);
+
 });
-
-
-
-/*
-getCode = function(e) {
-    var repos, req;
-    req = new XMLHttpRequest();
-    req.open('GET', 'https://api.github.com/user/repos');
-    req.setRequestHeader('Authorization', 'token 8f8f36e3b95c020f4bf7679c48609bac4576fec5');
-    req.onload = codeLoaded.bind(this);
-    req.send();
-
-    function codeLoaded() {
-        if (req.status == 200) {
-            repos = eval(req.response);
-            console.log(repos);
-        } else {
-            console.log('Some type of problem with the AJAX request!');
-            console.log(req); }
-
-        function populateMainbar(repos) {
-            var links, mainbar;
-            mainbar = document.getElementById('mainbar');
-            repos.forEach( function(repo) {
-                var link;
-                link = document.createElement('a');
-                link.id = repo.name;
-                link.setAttribute('class', 'repo');
-                mainbar.app
-                
-            });
-    }
-};
-*/
