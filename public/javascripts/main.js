a90bac92a36662a3be8d65cab89299c2dbe09a11
elju <more.silenter@gmail.com>
2013-07-29 17:14:11 -0400

    getting rid of unnecessary code and commenting

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,56 +19,32 @@ $(document).on('ready', function() {
 
     var $slices, $artwork, $activeHeap, undoInvert, pullUpViewer;
 
-    // if we clicked a link, then don't animate!!!
+    // if we clicked a link inside a subwindow, then make sure not to animate!!!
     $('.subwindow a').mousedown(function (e) {
         e.stopPropagation();
     });
 
-    $slices = $('.slice').attr('data-clicked', "false");
-    $slices.on('mousedown', function alternate (e) {
-        /*
-        var $this, $right, $left, $others;
+    $slices = $('.slice');
 
-        e.stopPropagation();
-        $('#warp').removeClass('go goone gotwo gothree gofour')
-        $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
-        */
+    // Now we basically do some routing.  If you click on a slice, then we want the page to zoom in on this slice.
+    // We do this by updating the hash, and catching it with the 'onhashchange' event later on.
+    $slices.on('mousedown', function alternate (e) {
         if (window.location.hash === $(this).attr('href')) {
-            // this.dataset.clicked = "false";
             window.location.hash = "#/";
         } else {
-            /*
-            $slices.attr('data-clicked', "false");
-            $right = $slices.filter('#' + this.id + ' ~ a');
-            $left = $slices.not($right).not(this);
-            $left.each(function(i) {
-                $(this).addClass('left' + ($left.length - i));} );
-            $right.each(function(i) {
-                $(this).addClass('right' + (i + 1)); });
-            $('#warp').addClass('go').addClass('go' + this.id);
-            focusOnSlice.apply(this, e);
-            */
-            $slices.attr('data-clicked', "false");
-            this.dataset.clicked = "true";
             var that = this;
             window.location.hash = $(this).attr('href');
         }
-        // focusOnSlice.apply(this, e);
     });
 
+    // Here is where all the magic happens.  (Well it a lot of it really also happens in lju.less)
     function focusOnSlice (e) {
         var $this, $right, $left, $others;
 
         // e.stopPropagation();
         $('#warp').removeClass('go goone gotwo gothree gofour')
         $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
-        if (window.location.hash === "#/" || window.location.hash === "") {
-            /*
-            this.dataset.clicked = "false";
-            window.location.hash = "#/";
-            */
-        } else {
-            // $slices.attr('data-clicked', "false");
+        if (window.location.hash !== "#/" && window.location.hash !== "") {
             $right = $slices.filter('#' + this.id + ' ~ a');
             $left = $slices.not($right).not(this);
             $left.each(function(i) {
@@ -76,81 +52,70 @@ $(document).on('ready', function() {
             $right.each(function(i) {
                 $(this).addClass('right' + (i + 1)); });
             $('#warp').addClass('go').addClass('go' + this.id);
-            /*
-            this.dataset.clicked = "true";
-            var that = this;
-            window.location.hash = $(this).attr('href');
-            */
         }
     }
 
     $('#navbar a[data-slice]').on('mousedown',
             function(e) {
-                e.preventDefault();
                 $(this.dataset.slice).mousedown();
             }
         );
 
     $('#logoAnchor').on('mousedown'
             , function(e) {
-                e.preventDefault();
                 window.location.hash = "#/";
-                // $('div[data-clicked="true"]').click();
             });
+
+    // Here's where we handle coming in to the page and going to a specific slice:
     switch (window.location.hash) {
         case "#/art":
             window.setTimeout(function() {
-                $('.slice[href="#/art"]').attr("data-clicked", "true");
                 focusOnSlice.apply($('.slice[href="#/art"]')[0]);
             }, 750);
             break;
         case "#/blog":
             window.setTimeout(function() {
-                $('.slice[href="#/blog"]').attr("data-clicked", "true");
                 focusOnSlice.apply($('.slice[href="#/blog"]')[0]);
             }, 750);
             break;
         case "#/other":
             window.setTimeout(function() {
-                $('.slice[href="#/other"]').attr("data-clicked", "true");
                 focusOnSlice.apply($('.slice[href="#/other"]')[0]);
             }, 750);
             break;
         case "#/code":
             window.setTimeout(function() {
-                $('.slice[href="#/code"]').attr("data-clicked", "true");
                 focusOnSlice.apply($('.slice[href="#/code"]')[0]);
             }, 750);
             break;
     }
-    $(window).on('hashchange', function(e) {
-        switch (window.location.hash) {
-            case "#/art":
-                $('.slice[href="#/art"]').attr("data-clicked", "true");
-                focusOnSlice.apply($('.slice[href="#/art"]')[0]);
-                break;
-            case "#/blog":
-                $('.slice[href="#/blog"]').attr("data-clicked", "true");
-                focusOnSlice.apply($('.slice[href="#/blog"]')[0]);
-                break;
-            case "#/other":
-                $('.slice[href="#/other"]').attr("data-clicked", "true");
-                focusOnSlice.apply($('.slice[href="#/other"]')[0]);
-                break;
-            case "#/code":
-                $('.slice[href="#/code"]').attr("data-clicked", "true");
-                focusOnSlice.apply($('.slice[href="#/code"]')[0]);
-                break;
-            case "#/":
-                $('#warp').removeClass('go goone gotwo gothree gofour')
-                $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
-                break;
-            case "":
-                $('#warp').removeClass('go goone gotwo gothree gofour')
-                $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
-                break;
-        }
+
+    // Here's the switchboard where we navigate to a new part of the page as the hash changes.
+    // I possibly could have done this by actually naming the slices in the hash and using the CSS selector :target, but some browsers
+    // such as Firefox have weird behavior when they try to center the element that is targeted in the hash of the url.
+    $(window).on('hashchange'
+            , function(e) {
+                switch (window.location.hash) {
+                    case "#/art":
+                        focusOnSlice.apply($('.slice[href="#/art"]')[0]);
+                        break;
+                    case "#/blog":
+                        focusOnSlice.apply($('.slice[href="#/blog"]')[0]);
+                        break;
+                    case "#/other":
+                        focusOnSlice.apply($('.slice[href="#/other"]')[0]);
+                        break;
+                    case "#/code":
+                        focusOnSlice.apply($('.slice[href="#/code"]')[0]);
+                        break;
+                    case "#/":
+                        $('#warp').removeClass('go goone gotwo gothree gofour')
+                        $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
+                        break;
+                    case "":
+                        $('#warp').removeClass('go goone gotwo gothree gofour')
+                        $slices.removeClass('right1 right2 right3 left1 left2 left3 scrollEnabled');
+                        break;
+                }
     });
-
-
 });
